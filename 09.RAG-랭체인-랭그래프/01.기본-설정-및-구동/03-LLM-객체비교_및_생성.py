33139ff2ee71c9ecd8897f347f2ddb366a76a40a
admin <admin@admin.com>
2026-07-03 14:18:46 +0900

    commit

--- 09.RAG-랭체인-랭그래프/01.기본-설정-및-구동/03-LLM-객체비교_및_생성.py
+++ 09.RAG-랭체인-랭그래프/01.기본-설정-및-구동/03-LLM-객체비교_및_생성.py
@@ -20,13 +20,29 @@ load_dotenv()
 # print(result)
 
 # 세번째 방법
-from langchain.chat_models import init_chat_model
+# from langchain.chat_models import init_chat_model
+# import os
+
+# model_name = os.getenv("LLM_AI_MODEL")
+# llm = init_chat_model(model_name)
+
+# respose = llm.invoke("오늘 날씨 어때?")
+# print(respose.content)
+# print(respose)
+
+import sys
+from pathlib import Path
 import os
 
-model_name = os.getenv("LLM_AI_MODEL")
-llm = init_chat_model(model_name)
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+#print(os.listdir(Path(__file__).resolve().parent.parent))
 
+from llm_loader import init_custom_llm
+
+print(init_custom_llm)
+
+llm = init_custom_llm()
 respose = llm.invoke("오늘 날씨 어때?")
+
 print(respose.content)
 print(respose)
-
