2bae88a1acb6a3f4799899a3255cea3ec3fddcf5
admin <admin@admin.com>
2026-07-06 11:01:10 +0900

    commit

--- 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/05-프롬프트-엔지니어링-변수화및-템플릿객체.py
+++ 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/05-프롬프트-엔지니어링-변수화및-템플릿객체.py
@@ -1,108 +1,25 @@
-
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from openai import OpenAI
 
-# 1. JSON 출력 강제하기
-# 일반출력
-# 결과가 자유로운 텍스트로 나옴(파싱 불가능)
-prompt = """
-사용자 정보를 만들어줘.
+# Prompt 변수화 (Template 구조)
+topic = "딥러닝"
 
-이름: 김철수
-나이: 25
-직업: 개발자
-"""
+# 💡 문제점
+# 코드와 Prompt가 섞임
+# 재사용 어려움
+prompt = f"""
+당신은 AI 강사입니다.
 
-# JSON 구조화 출력
-prompt = """
-다음 정보를 JSON 형식으로 출력하세요.
+다음 주제를 설명하세요:
+{topic}
 
 조건:
-- 반드시 JSON만 출력
-- 설명 금지
-- key는 영어로 작성
-
-정보:
-이름: 김철수
-나이: 25
-직업: 개
+- 초보자 대상
+- 예제 포함
+- 5줄 이내
 """
-
-# 실무형 JSON (강력 추천)
-prompt = """
-당신은 데이터 포맷터입니다.
-
-다음 정보를 JSON으로 변환하세요.
-
-[CONSTRAINT]
-- 반드시 JSON만 출력
-- 코드블록 사용 금지
-- 추가 설명 금지
-- null 금지
-
-[DATA]
-이름: 김철수
-나이: 25
-직업: 개발자
-경력: 3년
-"""
-# Table 출력 (보고서용)
-prompt = """
-다음 데이터를 표 형식으로 정리하세요.
-
-[DATA]
-Python, 중급, 3년
-Java, 초급, 1년
-C++, 고급, 5년
-
-[OUTPUT]
-Markdown table 형식으로 출력
-"""
-
-# API용 Strict Output (실전 필수)
-prompt = """
-당신은 API 응답 생성기입니다.
-
-[RULES]
-- JSON만 출력
-- 설명 금지
-- key는 snake_case 사용
-- 배열은 반드시 list로 출력
-
-[OUTPUT FORMAT]
-{
-  "user_name": "",
-  "user_age": 0,
-  "user_skills": []
-}
-
-[INPUT]
-이름: 김철수
-나이: 25
-기술: Python, AI, ML
-"""
-
-prompt = """
-당신은 AI 분석 에이전트입니다.
-
-[작업]
-사용자의 데이터를 분석하고 결론을 도출하세요.
-
-[절차]
-1. 문제 이해 (Reason)
-2. 해결 방법 선택 (Act)
-3. 결과 생성 (Output)
-4. 결과 검토 (Reflection)
-5. 개선 여부 판단
-
-[출력]
-- 분석 과정
-- 최종 결과
-- 검토 결과
-"""
-
 
 import sys
 from pathlib import Path
@@ -110,9 +27,34 @@ import os
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from llm_loader import init_custom_llm
+from langchain_core.prompts import PromptTemplate
+
+topic = "타이타닉 생존률"
+
+prompt = PromptTemplate.from_template("""
+당신은 데이터 분석 전문가입니다.
+
+주제: {topic}
+
+다음 조건으로 설명하세요:
+- 초보자도 이해 가능
+- 핵심만 설명
+- 예제 포함
+                                                                            
+""")
+result = prompt.invoke({"topic":"머신러닝"})
+#result = prompt.format(topic="반복문")
+
+print(type(result))
+print(result)
+
+prompt = ChatPromptTemplate.from_template("""
+                                                                              
+""")
+
 
 llm = init_custom_llm()
-respose = llm.invoke(prompt)
+respose = llm.invoke(result)
 
 print(respose.content)
 
