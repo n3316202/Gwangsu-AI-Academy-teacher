d889baa598c79128582ab1184a3f742622eb28bf
admin <admin@admin.com>
2026-07-20 11:51:03 +0900

    commit

--- 09.RAG-랭체인-랭그래프/07.랭그래프/실전-프로젝트/01.슈퍼바이저_패턴/01.버그수정_멀티_에이전트.py
+++ 09.RAG-랭체인-랭그래프/07.랭그래프/실전-프로젝트/01.슈퍼바이저_패턴/01.버그수정_멀티_에이전트.py
@@ -27,7 +27,7 @@ llm = init_custom_llm()
 
 # state 
 class State(TypedDict):
-    #대화 내역
+    #대화 내용
     messages:Annotated[list,add_messages]
 
     # Planner가 결정
@@ -96,3 +96,117 @@ def planner(state):
 # ----------------------------------------------------
 # Router
 # ----------------------------------------------------
+def router(state):
+
+    return state["next"]
+
+# ----------------------------------------------------
+# Debugger Agent
+# ----------------------------------------------------
+def debugger(state):
+
+    print("\n=============== Debugger =============")
+
+    messages = state["messages"]
+
+    result = llm.invoke(
+        messages + 
+        [
+            HumanMessage(content=
+    """
+    당신은 Python Debugger 입니다.
+
+    역할
+
+    1. 코드를 분석하세요.
+
+    2. 오류의 원인을 찾으세요.
+
+    3. 왜 오류가 발생했는지 설명하세요.
+
+    4. 수정 방향만 제안하세요.
+
+    주의
+
+    - 절대로 코드를 수정하지 마세요.
+    - 수정 코드는 작성하지 마세요.
+    - 원인 분석만 수행하세요.
+
+    출력 형식
+
+    ## 오류 원인
+
+    ...
+
+    ## 발생 이유
+
+    ...
+
+    ## 수정 방향
+
+    ...
+    """
+            )
+        ]
+    )
+
+    return {
+        "messages":[
+            AIMessage(
+                content = result.content,
+                name="Coder"
+            )
+        ],
+        "fix_done":True
+    }
+
+# ----------------------------------------------------
+# Supervisor
+# ----------------------------------------------------
+
+def supervisor(state):
+
+    print("\n=============== supervisor =============")
+
+    # 1단계
+    if not state["debug_done"]: # if state["debug_done"] == False
+        
+        print("Next -> debugger")
+        
+        return {
+            "next":"debugger"
+        }
+
+    return {
+        "next":"FINISH"
+    }
+# ----------------------------------------------------
+# Graph Builder
+# ----------------------------------------------------
+
+builder = StateGraph(State)
+
+builder.add_node("planner",planner)
+builder.add_node("supervisor",supervisor)
+builder.add_node("debugger",debugger)
+
+# ----------------------------------------------------
+# Edge
+# ----------------------------------------------------
+
+builder.add_edge(START,"planner")
+builder.add_edge("planner","supervisor")
+
+# ----------------------------------------------------
+# Conditional Routing
+# ----------------------------------------------------
+
+builder.add_conditional_edges(
+    "supervisor",
+    router,
+    {
+        "debugger":"debugger",
+        "FINISH":END
+    }
+
+)
