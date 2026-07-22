a05244760c03167a2dcfeb9a88721fd05fdd4c51
admin <admin@admin.com>
2026-07-22 16:01:39 +0900

    commit

--- 10.스트림릿/01.따라해보기.py
+++ 10.스트림릿/01.따라해보기.py
@@ -5,3 +5,9 @@ import pandas as pd
 # 제목
 ####################################################
 st.title("스트림릿 따라하기 : 허쌤")
+st.header("Header 예제")
+st.subheader("SubHeader 예제")
+
+
+st.text("안녕하세요")
+st.write("Streamlit을 배워봅시다!")
