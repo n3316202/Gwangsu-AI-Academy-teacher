1829c42ffd4a8ce044b9a550d6dfca08e09e14a8
admin <admin@admin.com>
2026-07-24 11:50:03 +0900

    commit

---
+++ 10.스트림릿/05.Form_사용법.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Form 예제")
+
+with st.form("폼태그"):
+    name = st.text_input("이름")
+    email = st.text_input("이메일")
+
+    submit = st.form_submit_button("가입")
+
+if submit:
+    st.success("제출완료")
+    st.write(name,email)
