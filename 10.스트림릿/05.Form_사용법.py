f81d4c491096e5b639976979027d4a59946079c3
admin <admin@admin.com>
2026-07-24 14:23:29 +0900

    commit

--- 10.스트림릿/05.Form_사용법.py
+++ 10.스트림릿/05.Form_사용법.py
@@ -8,23 +8,30 @@ with st.form("폼태그"):
 
     submit = st.form_submit_button("가입")
 
-import requests
 
-if submit:
-    st.success("제출완료")
-    st.write(name,email)
+# with 문법
+with open("test.txt","w",encoding="utf-8") as f:
+    f.write("안녕하세요!!")
 
 
-    response = requests.get(
-        "www.naver.com/login",
-        json = {
-            "name":name,
-            "email":email
-        }
-    )
 
-    if response.status_code == 200:
-        st.success("서버 전송 성공")
-    else:
-        st.error("전송실패")
+# import requests
+
+# if submit:
+#     st.success("제출완료")
+#     st.write(name,email)
+
+
+#     response = requests.get(
+#         "www.naver.com/login",
+#         json = {
+#             "name":name,
+#             "email":email
+#         }
+#     )
+
+#     if response.status_code == 200:
+#         st.success("서버 전송 성공")
+#     else:
+#         st.error("전송실패")
     
