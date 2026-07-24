461dfa1c5c2b51d4b480804f88b8eaf097c57922
admin <admin@admin.com>
2026-07-24 16:01:26 +0900

    commit

--- 10.스트림릿/05.Form_사용법.py
+++ 10.스트림릿/05.Form_사용법.py
@@ -8,11 +8,20 @@ with st.form("폼태그"):
 
     submit = st.form_submit_button("가입")
 
+# 파일처럼 자원을 닫기 위한 목적이 아니라, 
+# "현재 어떤 컨테이너(Container)에 위젯을 추가할 것인지"를 지정하는 역할
+
 
 # with 문법
 with open("test.txt","w",encoding="utf-8") as f:
     f.write("안녕하세요!!")
 
+f = open("test.txt", "w", encoding="utf-8")
+
+try:
+    f.write("안녕하세요.")
+finally:
+    f.close()
 
 
 # import requests
---
+++ 10.스트림릿/06.세션_캐쉬.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+# | 구분    | Session     | Cache          |
+# | ----- | ----------- | -------------- |
+# | 목적    | 사용자 상태 유지   | 작업 결과 재사용      |
+# | 기준    | 사용자별        | 앱/서버 기준        |
+# | 저장 내용 | 로그인 정보, 입력값 | 데이터, 모델, 계산 결과 |
+# | 공유    | 사용자마다 다름    | 여러 사용자가 공유 가능  |
+# | 변경 주체 | 사용자 행동      | 프로그램 실행        |
+# | 예     | 로그인 상태      | AI 모델          |
+
+st.set_page_config(
+    page_title="세션과캐쉬"
+)
+
+# =====================================================
+# Session State 초기화
+# =====================================================
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+if st.button("증가"):
+    st.session_state.count += 1 
+    #st.session_state.count = st.session_state.count + 1
+
+st.write("카운트=", st.session_state.count)
+
+st.divider()
+
+count = 0
+
+if st.button("증가-1"):
+    count += 1
+
+st.write("count =", count)
